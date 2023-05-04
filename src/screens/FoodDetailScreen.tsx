21e6e94821e1782176340dffc2ac0b53fd445cdf
Grchann <19521178>
2023-05-04 22:33:59 +0700

    add/navigate RestaurantScreen in FoodDetailScreen

--- src/screens/FoodDetailScreen.tsx
+++ src/screens/FoodDetailScreen.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect} from 'react'
 import { StyleSheet, View, Text, TouchableOpacity, TextInput, Image, ImageBackground, Dimensions } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler';
 import { ButtonWithIcon, FoodCard } from '../components';
-import { ApplicationState, FoodModel, onUpdateCart, UserState } from '../redux';
+import { ApplicationState, FoodModel, onUpdateCart, ShoppingState, UserState } from '../redux';
 import { connect } from 'react-redux';
 
 import { useNavigation, checkExistence } from '../utils'
@@ -12,13 +12,23 @@ interface FoodDetailProps{
     onUpdateCart: Function,
     navigation: { getParam: Function, goBack: Function}
     userReducer: UserState,
+    shoppingReducer: ShoppingState
  }
 
 const _FoodDetailScreen: React.FC<FoodDetailProps> = (props) => {
 
     const { getParam, goBack } = props.navigation;
 
-    const food = getParam('food') as FoodModel
+    const food = getParam('food') as FoodModel;
+
+    const restaurant = props.shoppingReducer.availability.restaurants.filter(restaurant=>
+        restaurant.foods.findIndex(searchFood=> searchFood._id === food._id) > -1
+    )[0]
+
+    const onTapRestaurantIcon = ()=>{
+        navigate('RestaurantPage', { restaurant: restaurant})
+    }
+    
 
     const { navigate } = useNavigation()
 
@@ -28,6 +38,7 @@ const _FoodDetailScreen: React.FC<FoodDetailProps> = (props) => {
 return (<View style={styles.container}>
         <View style={styles.navigation}>
             <ButtonWithIcon icon={require('../images/back_arrow.png')} onTap={() => goBack()} width={30} height={30} />
+            <ButtonWithIcon icon={require('../images/food-stall.png')} onTap={() => onTapRestaurantIcon()} width={30} height={30} />
         </View>
         <View style={styles.body}>
             <ImageBackground source={{ uri: `${food.images[0]}`}}
@@ -54,7 +65,7 @@ return (<View style={styles.container}>
 
 const styles = StyleSheet.create({
 container: { flex: 1, backgroundColor: '#F2F2F2'},
-navigation: { flex: 1, marginTop: 43, paddingLeft: 10, flexDirection: 'row', alignItems: 'center' },
+navigation: { flex: 1, marginTop: 43, paddingLeft: 10, paddingRight: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' },
 body: { flex: 10, justifyContent: 'flex-start', alignItems: 'center', backgroundColor: '#FFF', paddingBottom: 160 },
 footer: { flex: 1, backgroundColor: 'cyan' }
 })
@@ -62,6 +73,7 @@ footer: { flex: 1, backgroundColor: 'cyan' }
 
 const mapToStateProps = (state: ApplicationState) => ({
     userReducer: state.userReducer,
+    shoppingReducer: state.shoppingReducer
 })
 
 const FoodDetailScreen = connect(mapToStateProps, { onUpdateCart })(_FoodDetailScreen) 
