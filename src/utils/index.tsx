ce76f39fbcd8376f7accb92f076456ec99d0756c
Grchann <19521178>
2023-05-12 20:39:22 +0700

    test/gitignore

--- src/utils/index.tsx
+++
@@ -1,8 +0,0 @@
-
-// export const BASE_URL = "http://192.168.1.7:8888/"
-export const BASE_URL = "http://192.168.127.94:8888/"
-
-export const MAP_API_KEY = 'NEW_API_KEY'
-
-export * from './useNavigation'
-export * from './CartHelper';
